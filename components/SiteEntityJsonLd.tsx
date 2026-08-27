6e9820e2c576dd77e1d2e8028c9d08e77bbdc543
unikmo <mbanwie@googlemail.com>
2026-08-27 15:40:39 +0200

    Add sitewide UNIKMO entity graph for AI discovery

---
+++ components/SiteEntityJsonLd.tsx
@@ -0,0 +1,49 @@
+export default function SiteEntityJsonLd() {
+  const entityGraph = {
+    '@context': 'https://schema.org',
+    '@graph': [
+      {
+        '@type': 'Organization',
+        '@id': 'https://www.unikmo.com/#organization',
+        name: 'UNIKMO',
+        url: 'https://www.unikmo.com/',
+        description:
+          'UNIKMO creates physical keepsake cards that unlock a private digital memory such as a video, voice note, photo, or written message.',
+        logo: {
+          '@type': 'ImageObject',
+          '@id': 'https://www.unikmo.com/#logo',
+          url: 'https://www.unikmo.com/unikmo-logo-header.png',
+          contentUrl: 'https://www.unikmo.com/unikmo-logo-header.png',
+          caption: 'UNIKMO',
+        },
+        brand: { '@id': 'https://www.unikmo.com/#brand' },
+      },
+      {
+        '@type': 'Brand',
+        '@id': 'https://www.unikmo.com/#brand',
+        name: 'UNIKMO',
+        url: 'https://www.unikmo.com/',
+        description:
+          'A physical card that gives someone a private route back to a video, voice note, photo, or written message.',
+        logo: { '@id': 'https://www.unikmo.com/#logo' },
+      },
+      {
+        '@type': 'WebSite',
+        '@id': 'https://www.unikmo.com/#website',
+        url: 'https://www.unikmo.com/',
+        name: 'UNIKMO',
+        description:
+          'Official website for UNIKMO physical memory cards and private digital moments.',
+        publisher: { '@id': 'https://www.unikmo.com/#organization' },
+        inLanguage: 'en',
+      },
+    ],
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(entityGraph) }}
+    />
+  );
+}
