884c01b5e834d7578083fffe463d8cce9858ceb6
unikmo <mbanwie@googlemail.com>
2026-08-27 15:43:42 +0200

    Make AI search crawler access explicit while protecting private routes

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,29 @@
 import type { MetadataRoute } from 'next';
 
+const privateRoutes = ['/admin', '/api', '/unlock', '/upload'];
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin', '/api', '/unlock', '/upload'],
+        disallow: privateRoutes,
+      },
+      {
+        userAgent: 'OAI-SearchBot',
+        allow: '/',
+        disallow: privateRoutes,
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+        disallow: privateRoutes,
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+        disallow: privateRoutes,
       },
     ],
     sitemap: 'https://www.unikmo.com/sitemap.xml',
